4630971e7a68e8602857be688be6bcf6a1748e68
Marc Hauschildt <marc.hauschildt@gmail.com>
2019-02-15 10:30:26 -0600

    Setup single-page app using page.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+
+  module.showOnly = (selector) => {
+    $('.main-section').hide();
+    $(selector).show();
+  };
+
+})(app);
---
+++ js/routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+page('/', () => app.showOnly('.home-page'));
+page('/processing', () => app.showOnly('.processing-page'));
+page('/products', () => app.showOnly('.products-page'));
+page('/about', () => app.showOnly('.about-page'));
+page('/testimonials', () => app.showOnly('.testimonials-page'));
+page('/contact', () => app.showOnly('.contact-page'));
+
+page();
